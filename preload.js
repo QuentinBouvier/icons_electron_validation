f453940b696d1ddab105e3d93781e25c4d010d7c
Quentin Bouvier <32564163+QuentinBouvier@users.noreply.github.com>
2019-09-26 09:45:35 +0200

    feat: Set startup env
    Add libraries and framework
    
    Import the templating service from previous project

--- preload.js
+++ preload.js
@@ -1,12 +1,44 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+const fs = require('fs');
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
     if (element) element.innerText = text
-  } 
-  
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
   }
+
+  displayComponent('index', () => {
+      document.getElementById('component').innerHTML += loadTemplate('versions', {});
+
+      for (const type of ['chrome', 'node', 'electron']) {
+        replaceText(`${type}-version`, process.versions[type])
+      }
+  })
+
 })
+
+function displayComponent(componentName, callback) {
+    const body = document.getElementById('component');
+    fs.readFile(`components/${componentName}.component.html`, 'utf8', (err, file) => {
+        if (!err) {
+            body.innerHTML = file;
+            if (typeof callback === 'function')
+                callback();
+        }
+        else {
+            body.innerHTML = err;
+        }
+    });
+}
+
+function loadTemplate(templateName, values) {
+    template = fs.readFileSync(`./components/templates/${templateName}.tpl.html`, 'utf8');
+
+    for (let v in values) {
+        let replaceTag = new RegExp(`{{${v}}}`, 'g');
+        template = template.replace(replaceTag, values[v]);
+    }
+
+    return template;
+}
